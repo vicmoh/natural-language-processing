f70d1d200b34ca739afa64b8defa6acdf23c8e53
Vicky Mohammad <vimoh1213@gmail.com>
2019-09-28 21:17:10 -0400

    [AUTO]

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,30 @@
+import java.io.InputStreamReader;
+
 /**
  * This class is the main class to run the program.
  */
 public class Main {
+	/**
+	 * The default path of for testing a sample file.
+	 */
+	private static final String DEFAULT_FILE_PATH = "../assets/samples.text";
+
 	/**
 	 * Main functions to run the program
 	 * 
 	 * @param args
 	 */
 	public static void main(String args[]) {
-		System.out.println("\nRunning program...");
 		// Check if args exist
-		if (args.length <= 1) {
-			System.out.println("Argument is empty.");
-			return;
-		}
+		String fileName;
+		if (args.length <= 1)
+			fileName = DEFAULT_FILE_PATH;
+		else
+			fileName = args[0];
 		// If arg exist
-		System.out.println("File is \"" + args[0] + "\"");
+		System.out.println("\nRunning program with file path: \"" + fileName + "\".");
 		try {
-			readFile("../assets/samples.text");
+			readFile(fileName);
 		} catch (Exception error) {
 			System.out.println(error);
 		}
@@ -26,7 +33,6 @@ public class Main {
 	private static void readFile(String fileName) throws Exception {
 		if (fileName == null)
 			throw new Exception("File name is not passed.");
-		String debug = "Main.readFile():";
-		System.out.println(debug + " Invoked.");
+
 	}
 }
